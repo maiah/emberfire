0ddce584685bbe6da26f59a85afbe29fe6e179ec
Maiah Macariola <jjmmacariola@gmail.com>
2012-05-06 12:39:31 +0800

    added autosync of read and update operations of emberjs to firebase

--- app.js
+++ app.js
@@ -1,9 +1,13 @@
 ﻿$(function() {
 window.App = Ember.Application.create();
-var firebaseLocation = "/xxxxx";
+var firebaseLocation = "/your_root_location_here";
 
 Person = EmberFire.extend({
-  locationUrl: firebaseLocation + "/person",
+  emberFireConf: {
+    locationUrl: firebaseLocation + "/person",
+    isList: false
+  },
+
   firstName: "",
   lastName: "",
   age: 0,
@@ -12,5 +16,25 @@ Person = EmberFire.extend({
 
 App.person = Person.create();
 App.person.initialize();
+
+var userConf = {
+    locationUrl: firebaseLocation + "/users/maiah",
+    isList: false
+}
+
+User = EmberFire.extend({
+  emberFireConf: userConf,
+
+  name: {
+    first: "",
+    last: "",
+  },
+  groups: [{
+    "id" : {name: ""}
+  }]
+});
+
+App.user = User.create();
+App.user.initialize();
 
 });
--- emberfire.js
+++ emberfire.js
@@ -1,20 +1,39 @@
 FirebaseServer = "http://gamma.firebase.com";
 
 EmberFire = Ember.Object.extend({
-  locationUrl: "/",
+  emberFireConf: {
+    locationUrl: "/",
+    isList: false
+  },
 
   firebaseLocation: function() {
-    return new Firebase(FirebaseServer + this.locationUrl);
+    return new Firebase(FirebaseServer + this.emberFireConf.locationUrl);
   },
 
   initialize: function() {
-    console.log("Initializing");
+    var readEvent = this.emberFireConf.isList ? "child_added" : "value";
+
+    // Get the firebase object
     var instance = this;
-    instance.firebaseLocation().once("value", function(snapshot) {
+    this.firebaseLocation().on("value", function(snapshot) {
+      var props = {};
       var val = snapshot.val();
       for (var prop in val) {
         instance.set(prop, val[prop]);
+        props[prop] = "";
+      }
+
+      // Add observers on the emberjs object
+      for (var prop in props) {
+        instance.addObserver(prop, function() {
+          var values = {};
+          for (var p in props) {
+            values[p] = instance.get(p);
+          }
+          instance.firebaseLocation().set(values);
+        });
       }
     });
   }
+
 });
